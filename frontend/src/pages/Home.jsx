7c23d9ff3ea0cea4a665ec61fffd173379e67d87
Erfan Heshmati <heshmati74@gmail.com>
2024-05-14 16:13:24 +0330

    about section ui design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import Hero from "../components/Hero";
 import HowItWorks from "../components/HowItWorks";
 
@@ -6,6 +7,7 @@ export default function Home() {
         <>
             <Hero />
             <HowItWorks />
+            <About />
         </>
     )
 }
