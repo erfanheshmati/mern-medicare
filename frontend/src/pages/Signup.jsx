2e724cf8f50963bdfe36e172ea3cc1e287350bb7
Erfan Heshmati <heshmati74@gmail.com>
2024-05-16 20:24:32 +0330

    signup page ui design

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,140 @@
+import signupImg from "../assets/images/signup.gif"
+import avatar from "../assets/images/doctor-img01.png"
+import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function Signup() {
+
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [previewURL, setPreviewURL] = useState("")
+
+    const [formData, setFormData] = useState({ name: "", email: "", password: "", role: "", gender: "", photo: selectedFile })
+
+    const handleInputChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleFileInputChange = async (e) => {
+        const file = e.target.files[0]
+        // we will use cloudinary to upload image later
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Signup
-        </div>
+        <section className="px-5 xl:px-0">
+            <div className="max-w-[1170px] mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                    {/* Image */}
+                    <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+                        <figure className="rounded-l-lg">
+                            <img src={signupImg} alt="" className="w-full rounded-l-lg" />
+                        </figure>
+                    </div>
+                    {/* Form */}
+                    <div className="rounded-l-lg lg:pl-16 py-10">
+                        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+                            Create an
+                            <span className="text-primaryColor"> account</span>
+                        </h3>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-5">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Full Name"
+                                    value={formData.name}
+                                    onChange={handleInputChange}
+                                    className="w-full px-1 py-3 border-b border-solid border-[#0066ff40] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-5">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email Address"
+                                    value={formData.email}
+                                    onChange={handleInputChange}
+                                    className="w-full px-1 py-3 border-b border-solid border-[#0066ff40] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-5">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    value={formData.password}
+                                    onChange={handleInputChange}
+                                    className="w-full px-1 py-3 border-b border-solid border-[#0066ff40] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-5 flex items-center justify-between">
+                                <label className="text-headingColor font-bold text-[16px] leading-7">
+                                    Role:
+                                    <select
+                                        name="role"
+                                        value={formData.role}
+                                        onChange={handleInputChange}
+                                        className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                                    >
+                                        <option value="">Select</option>
+                                        <option value="patient">Patient</option>
+                                        <option value="doctor">Doctor</option>
+                                    </select>
+                                </label>
+                                <label className="text-headingColor font-bold text-[16px] leading-7">
+                                    Gender:
+                                    <select
+                                        name="gender"
+                                        value={formData.gender}
+                                        onChange={handleInputChange}
+                                        className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                                    >
+                                        <option value="">Select</option>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </label>
+                            </div>
+                            <div className="mb-5 flex items-center gap-3">
+                                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                                    <img src={avatar} alt="" className="w-full rounded-full" />
+                                </figure>
+                                <div className="relative w-[130px] h-[50px]">
+                                    <input
+                                        type="file"
+                                        name="photo"
+                                        id="customFile"
+                                        accept=".jpg, .jpeg, .png"
+                                        onChange={handleFileInputChange}
+                                        className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                                    />
+                                    <label
+                                        htmlFor="customFile"
+                                        className="absolute top-0 left-0 w-full h-full flex items-center px-4 py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer">
+                                        Upload Photo
+                                    </label>
+                                </div>
+                            </div>
+                            <div>
+                                <button type="submit" className="btn w-full rounded-lg text-[18px]">
+                                    Sign Up
+                                </button>
+                            </div>
+                            <p className="mt-5 text-textColor text-center">
+                                Already have an account?
+                                <Link to="/login" className="text-primaryColor font-medium ml-1">Login</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
