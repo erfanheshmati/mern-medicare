4703f3a7a437098c30a6846db8c692c9f488b6d8
Erfan Heshmati <heshmati74@gmail.com>
2024-05-18 13:59:30 +0330

    login api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,12 @@ import Doctor from "../models/doctorModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+};
+
 export const register = async (req, res) => {
   const { name, email, password, role, gender, photo } = req.body;
 
@@ -50,6 +56,38 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { email } = req.body;
+
   try {
-  } catch (error) {}
+    let user = null;
+
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+
+    if (patient) user = patient;
+    if (doctor) user = doctor;
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // password comparison
+    const isMatch = bcrypt.compare(req.body.password, user.password);
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ status: false, message: "Invalid Credentials" });
+
+    // get token
+    const token = generateToken(user);
+
+    const { password, role, appointments, ...rest } = user._doc;
+    res.status(200).json({
+      status: true,
+      message: "Login successfully",
+      token,
+      data: { ...rest },
+      role,
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Login failed" });
+  }
 };
