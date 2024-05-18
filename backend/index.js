7a9d2265d8dfbdd9ba6f684876949f752ad1183b
Erfan Heshmati <heshmati74@gmail.com>
2024-05-18 12:41:05 +0330

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true,
+};
+
+app.get("/", (req, res) => {
+  res.send("api is working");
+});
+
+// Database Connection
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB database is connected");
+  } catch (error) {
+    console.log("MongoDB database connection is failed");
+  }
+};
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  connectDB();
+  console.log("Server is running on port " + port);
+});
