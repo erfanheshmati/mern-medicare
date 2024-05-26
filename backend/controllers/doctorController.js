bce540a869d569645acc0fe2301ddc17545f4d22
Erfan Heshmati <heshmati74@gmail.com>
2024-05-26 12:36:07 +0330

    doctor profile api

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,5 @@
 import Doctor from "../models/doctorModel.js";
+import Booking from "../models/bookingModel.js";
 
 export const updateDoctor = async (req, res) => {
   const id = req.params.id;
@@ -71,5 +72,29 @@ export const getAllDoctors = async (req, res) => {
     });
   } catch (error) {
     res.status(404).json({ success: false, message: "Doctors not found" });
+  }
+};
+
+export const getDoctorProfile = async (req, res) => {
+  const doctorId = req.userId;
+  try {
+    const doctor = await Doctor.findById(doctorId);
+    if (!doctor) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Doctor not found" });
+    }
+    const { password, ...rest } = doctor._doc;
+    const appointments = await Booking.findById({ doctor: doctorId });
+    res.status(200).json({
+      success: true,
+      message: "Profile info is getting",
+      data: { ...rest, appointments },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong, Can not get data",
+    });
   }
 };
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -4,6 +4,7 @@ import {
   deleteDoctor,
   getSingleDoctor,
   getAllDoctors,
+  getDoctorProfile,
 } from "../controllers/doctorController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js";
 import reviewRouter from "./reviewRoute.js";
@@ -17,5 +18,6 @@ router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctors);
 router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
 router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
+router.get("/profile/me", authenticate, restrict(["doctor"]), getDoctorProfile);
 
 export default router;
